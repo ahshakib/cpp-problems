99988add3072cab0c645789d912754727aa9c2cb
ahshakib <ahshakib75@gmail.com>
2025-01-09 10:54:46 +0600

    Add sentence input programme in c++

---
+++ inputSentence.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    string s;
+    getline(cin, s);
+    cout<<s<<endl;
+}
+
+/*
+   input: My name is Shakib
+   output: My name is Shakib
+*/
